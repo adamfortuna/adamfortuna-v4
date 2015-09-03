08236b86e9f804796ce9cbc15d0b2c3d210e82cf
Adam Fortuna <adam@fortuna.name>
2015-09-02 22:39:40 -0400

    Switching to same disqus permalinks at production

--- helpers/article_helpers.rb
+++ helpers/article_helpers.rb
@@ -1,5 +1,20 @@
 module ArticleHelpers
 
+  def disqus_shortname
+    data.config.disqus[environment]
+  end
+
+  def disqus_permalink commentable
+    "#{data.config.url}#{commentable.url}"
+  end
+  # "#{data.config.url}/articles/#{commentable.data.permalink}"
+
+  def disqus_identifier commentable
+    return commentable.data['disqus_identifier'] if commentable.data['disqus_identifier']
+
+    "#{data.config.disqus.identifier}-/#{commentable.data['permalink']}"
+  end
+
   def classes_from_offset large_offset, medium_offset=nil, small_offset=nil
     large_offset ||= 2
     large_columns = 12 - large_offset
