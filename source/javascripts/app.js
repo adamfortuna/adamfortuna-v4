202b23c9caef81f2bf6623d1ac64736bbf374c7a
Adam Fortuna <adam@fortuna.name>
2016-02-17 20:45:00 -0500

    Added trolling audio

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -7,3 +7,23 @@
 $(function() {
   $('body').addClass('is-loaded');
 });
+
+window.speaking = false;
+
+function troll() {
+  console.log(window.speaking);
+  if(!window.speaking) {
+    window.speaking = true;
+    // paste in your console
+    speechSynthesis.onvoiceschanged = function() {
+      var msg = new SpeechSynthesisUtterance();
+      msg.voice = this.getVoices().filter(v => v.name == 'Cellos')[0];
+      content = document.getElementsByTagName('body')[0].textContent.replace(/^\n/mg, '').split('\n').splice(5, 100).join(' ').replace(/\.|\!|\,/mg, ' ')
+      msg.text = content;
+      this.speak(msg);
+    };
+  } else {
+    window.speaking = false;
+    speechSynthesis.pause();
+  }
+}
