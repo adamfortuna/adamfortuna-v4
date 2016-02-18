32d87d507b80c5799848ff244af1157fba78a0ea
Adam Fortuna <adam@fortuna.name>
2016-02-17 21:05:58 -0500

    Only for chrome

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -6,6 +6,10 @@
 
 $(function() {
   $('body').addClass('is-loaded');
+
+  if(!window.chrome) {
+    $('.troll').hide();
+  }
 });
 
 window.speaking = false;
@@ -17,8 +21,20 @@ function troll() {
     // paste in your console
     speechSynthesis.onvoiceschanged = function() {
       var msg = new SpeechSynthesisUtterance();
-      msg.voice = this.getVoices().filter(v => v.name == 'Cellos')[0];
-      content = document.getElementsByTagName('body')[0].textContent.replace(/^\n/mg, '').split('\n').splice(5, 100).join(' ').replace(/\.|\!|\,/mg, ' ')
+      msg.voice = this.getVoices().filter(function(v) { return v.name == 'Cellos'; })[0];
+      var content = document.getElementsByTagName('body')[0].textContent.replace(/^\n/mg, '').split(/\s\s/);
+      if(content.length < 100) {
+        content = content[0].split('\n')
+      }
+      content = content.join(' ').replace(/\.|\!|\,/mg, ' ').replace(/\s{2,}/mg, ' ').split(' ');
+      var index = 0;
+      for(var i=0, len=content.length; i<len; i++) {
+        if(content[i] === 'Photos') {
+          index = i;
+          break;
+        }
+      }
+      content = content.splice(i+1, content.length).join(' ');
       msg.text = content;
       this.speak(msg);
     };
